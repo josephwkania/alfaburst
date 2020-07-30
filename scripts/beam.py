8db888033e1bbb05bc8b61e3088dc10c44da837e
josephwkania <60994292+josephwkania@users.noreply.github.com>
2020-07-30 17:12:56 -0400

    refactored beam0.py to beam.py, beam.py does not restart a running watcher

--- scripts/beam.py
+++ scripts/beam.py
@@ -5,15 +5,33 @@ from circus.watcher import Watcher
 import logging
 import pika
 import subprocess
+import yaml
+
 logger = logging.getLogger()
 format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
 logging.basicConfig(level=logging.INFO, format=format)
 logging.getLogger('pika').setLevel(logging.INFO)
 
-watcher = Watcher('cheeta', '/home/artemis/development/arecibo/dev/build/linux/gcc/release/work/arecibo/alfaburst_16bit --config /home/artemis/configs/beam0.xml -p empty -s udp')
-#watcher to run the data recorder, keep as a global variable
+with open("/home/artemis/programs/alfaburst/scripts/config/conf.yaml", 'r') as stream:
+    data_loaded = yaml.load(stream)
+    login_detail = data_loaded['rabbit']
+    headnode = login_detail['headnode']
+    user = login_detail['user']
+    password = login_detail['password']
 
-def record_fil(status):
+
+def create_watcher(beam):
+    """
+    creates a global watcher object for a specified beam
+
+    Parameters:
+    beam (int): the beam number to make a watcher 
+    """
+    global watcher
+    watcher = Watcher('cheeta', f'/home/artemis/development/arecibo/dev/build/linux/gcc/release/work/arecibo/alfaburst_16bit --config /home/artemis/configs/beam{beam}.xml -p empty -s udp')
+    #watcher to run the data recorder, keep as a global variable
+
+def record_fil(status, beam):
     """
     Starts and stops the Watcher that runs the cheeta filterbank recorder
 
@@ -21,27 +39,30 @@ def record_fil(status):
     status (string): a 'True'/'False' string the determines if data should be taken
     """   
     if status == 'True':
+        print(watcher.status)
         logging.info('Recieved True, starting watcher')
         try:
-            watcher.reload() #use reload incase the watcher had been previously stopped
+            print('Starting watcher')
+            watcher.start()
         except Exception as e:
             logging.info(f'Watcher cannot be started: {e}')
     else:
         logging.info('Recieved False, stopping watcher')
         try:
-            watcher.kill_processes()
+            watcher.stop()
+            create_watcher(beam) #create a new watcher b/c we can't revive the old one
         except Exception as e:
             logging.info(f'Arbiter cannot be stopped: {e}')
 
-def stage_initer():
+def stage_initer(beam):
     """
     Communicates with AMQP on the head node to recieve start ('True') and stop ('False') signals to start and stop recording data
     """
-    credentials = pika.PlainCredentials('artemis','artemis')
-    connection = pika.BlockingConnection(pika.ConnectionParameters('10.1.1.1', 5672, '/', credentials))
+    credentials = pika.PlainCredentials(user,password)
+    connection = pika.BlockingConnection(pika.ConnectionParameters(headnode, 5672, '/', credentials))
     channel = connection.channel()
     
-    channel.queue_declare(queue='recorder_beam0', durable=True)
+    channel.queue_declare(queue=f'recorder_beam{beam}', durable=True)
     
     def callback(ch, method, properties, body):
         """
@@ -50,12 +71,12 @@ def stage_initer():
         status = body.decode()
         logging.info(f'got it {status}')
         ch.basic_ack(delivery_tag = method.delivery_tag)
-        record_fil(status)
+        watcher = record_fil(status, beam)
         logging.info("Ack'ed")
     
     channel.basic_qos(prefetch_count=1)
     channel.basic_consume(on_message_callback=callback,
-                          queue='recorder_beam0')
+                          queue=f'recorder_beam{beam}')
     try:    
         channel.start_consuming()
     except (StreamLostError, ConnectionResetError) as e:
@@ -77,15 +98,19 @@ def begin_main(values):
 
 if __name__ == "__main__":
     parser=ArgumentParser(description='Record data with the Cheeta Recorder', formatter_class=ArgumentDefaultsHelpFormatter)
+    parser.add_argument('-b', '--beam', dest='beam', type=int, help='sets the beam number, default: 0')
     parser.add_argument('-v', '--verbose', dest='verbose', action='store_true', help='Be verbose')
     parser.add_argument('-d', '--daemon', dest='daemon', action='store_false', help='Run with AMQP')
+    parser.set_defaults(beam=0)
     parser.set_defaults(verbose=False)
     parser.set_defaults(daemon=True)
     values = parser.parse_args()
-    
+
+    create_watcher(values.beam)
+
     if values.daemon:
         logging.info('Running in daemon mode')
-        stage_initer()
+        stage_initer(values.beam)
     else:
         begin_main(values)
 
